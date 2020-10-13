54c5561e5c487b65e13b26c9e8bdce1f3736095b
Konstantin Dorogan <sayd3x@users.noreply.github.com>
2020-10-14 00:08:51 +0300

    SampleApp integrated into the Sdk workspace

--- SampleApp/SampleApp/ItlyFixtures.swift
+++ SampleApp/SampleApp/ItlyFixtures.swift
@@ -34,13 +34,13 @@ import ItlySchemaValidatorPlugin
 
 
 @objc extension ItlySchemaValidatorPlugin {
-    @objc convenience init() throws {
+    @objc override convenience init() {
         let schemas: [String: Data] = [
             "context" : "{\"$id\":\"https://iterative.ly/company/77b37977-cb3a-42eb-bce3-09f5f7c3adb7/context\",\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"title\":\"Context\",\"description\":\"\",\"type\":\"object\",\"properties\":{\"requiredString\":{\"description\":\"description for context requiredString\",\"type\":\"string\"},\"optionalEnum\":{\"description\":\"description for context optionalEnum\",\"enum\":[\"Value 1\",\"Value 2\"]}},\"additionalProperties\":false,\"required\":[\"requiredString\"]}",
             "group" : "{\"$id\":\"https://iterative.ly/company/77b37977-cb3a-42eb-bce3-09f5f7c3adb7/group\",\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"title\":\"Group\",\"description\":\"\",\"type\":\"object\",\"properties\":{\"requiredBoolean\":{\"description\":\"Description for group requiredBoolean\",\"type\":\"boolean\"},\"optionalString\":{\"description\":\"Description for group optionalString\",\"type\":\"string\"}},\"additionalProperties\":false,\"required\":[\"requiredBoolean\"]}",
             "identify" : "{\"$id\":\"https://iterative.ly/company/77b37977-cb3a-42eb-bce3-09f5f7c3adb7/identify\",\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"title\":\"Identify\",\"description\":\"\",\"type\":\"object\",\"properties\":{\"optionalArray\":{\"description\":\"Description for identify optionalArray\",\"type\":\"array\",\"uniqueItems\":false,\"items\":{\"type\":\"string\"}},\"requiredNumber\":{\"description\":\"Description for identify requiredNumber\",\"type\":\"number\"}},\"additionalProperties\":false,\"required\":[\"requiredNumber\"]}"
         ].compactMapValues{ $0.data(using: .utf8) }
-        try self.init(schemasMap: schemas)
+        try! self.init(schemasMap: schemas)
     }
 }
 
@@ -54,20 +54,20 @@ import ItlySchemaValidatorPlugin
         case value2 = "Value 2"
     }
     
-    init(requiredString: String, optionalEnum: OptionalEnum? = nil) {
-        super.init(name: "context", properties: ["requiredString": requiredString, "optionalEnum": optionalEnum?.rawValue].compactMapValues{ $0 })
+    convenience init(requiredString: String, optionalEnum: OptionalEnum? = nil) {
+        self.init(name: "context", properties: ["requiredString": requiredString, "optionalEnum": optionalEnum?.rawValue].compactMapValues{ $0 })
     }
 }
 
 @objc class Identify: Event {
     @objc static let VALID_ALL_PROPS = Identify(requiredNumber: 2.0, optionalArray: ["optional"])
     
-    @objc init(requiredNumber: Double, optionalArray: [String]? = nil) {
+    @objc convenience init(requiredNumber: Double, optionalArray: [String]? = nil) {
         var dict:[String:Any] = ["requiredNumber": requiredNumber]
         if let optionalArray = optionalArray {
             dict["optionalArray"] = optionalArray
         }
-        super.init(name: "identify", properties: dict)
+        self.init(name: "identify", properties: dict)
     }
 }
 
@@ -75,17 +75,17 @@ import ItlySchemaValidatorPlugin
     @objc static let VALID_ALL_PROPS = Group(requiredBoolean: false, optionalString: "I'm optional!")
     
     
-    @objc init(requiredBoolean: Bool, optionalString: String? = nil) {
+    @objc convenience init(requiredBoolean: Bool, optionalString: String? = nil) {
         var dict:[String:Any] = ["requiredBoolean": requiredBoolean]
         if let optionalString = optionalString {
             dict["optionalString"] = optionalString
         }
-        super.init(name: "group", properties: dict)
+        self.init(name: "group", properties: dict)
     }
 }
 
 @objc class EventNoProperties: Event {
-    @objc init() {
-        super.init(name: "Event No Properties")
+    @objc convenience override init() {
+        self.init(name: "Event No Properties")
     }
 }
