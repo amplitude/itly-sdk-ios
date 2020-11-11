f2ee8f67968ae5f6deef3a180af7f5d8ffc7ce7c
Justin Fiedler <justin@iterative.ly>
2020-11-11 21:46:10 +0000

    Added Options convenience methods (builderBlock, withOverrides)

--- Sdk/Sdk/Options.swift
+++ Sdk/Sdk/Options.swift
@@ -1,12 +1,25 @@
 //
 //  Options.swift
-//  ItlyCore
+//  Options for Itly
 //
-//  Created by Konstantin Dorogan on 14.09.2020.
-//  Copyright © 2020 Konstantin Dorogan. All rights reserved.
+//  Copyright © 2020 Iteratively. All rights reserved.
 //
 
 import Foundation
+
+// DEFAULTS
+public let DEFAULT_ENVIRONMENT: Environment = .development
+public let DEFAULT_DISABLED: Bool = false
+public let DEFAULT_PLUGINS: [Plugin] = []
+public let DEFAULT_LOGGER: Logger? = nil
+public let DEFAULT_VALIDATION_OPTIONS: ValidationOptions? = nil
+public let VALIDATION_OPTIONS_DEV: ValidationOptions = ValidationOptions(
+    disabled: false, trackInvalid: false, errorOnInvalid: true
+)
+public let VALIDATION_OPTIONS_PROD: ValidationOptions = ValidationOptions(
+    disabled: false, trackInvalid: true, errorOnInvalid: false
+)
+
 
 @objc (ITLItlyOptions) open class Options: NSObject {
     @objc public let environment: Environment
@@ -15,84 +28,63 @@ import Foundation
     @objc public let validation: ValidationOptions
     @objc public let logger: Logger?
 
-    @objc public init(environment: Environment = .development,
-                disabled: Bool = false,
-                plugins: [Plugin] = [],
-                validation: ValidationOptions? = nil,
-                logger: Logger? = nil
+    @objc public init(
+        environment: Environment = DEFAULT_ENVIRONMENT,
+        disabled: Bool = DEFAULT_DISABLED,
+        plugins: [Plugin] = DEFAULT_PLUGINS,
+        validation: ValidationOptions? = DEFAULT_VALIDATION_OPTIONS,
+        logger: Logger? = DEFAULT_LOGGER
     ) {
         self.environment = environment
         self.disabled = disabled
         self.plugins = plugins
-        self.validation = validation ?? ValidationOptions(trackInvalid: environment == .production, errorOnInvalid: environment != .production)
+        self.validation = validation ?? ((environment == .production)
+            ? VALIDATION_OPTIONS_PROD
+            : VALIDATION_OPTIONS_DEV)
         self.logger = logger
         super.init()
     }
-}
-
-@objc public extension Options {
-    @objc func withOverrides(environment: Environment,
-                             disabled: NSNumber? = nil,
-                             plugins: [Plugin]? = nil,
-                             validation: ValidationOptions? = nil,
-                             logger: Logger? = nil
+    
+    @objc public func withOverrides(
+        _ builderBlock: (OptionsBuilder) -> Void
     ) -> Options {
-        return Options(
-            environment: environment,
-            disabled: disabled?.boolValue ?? self.disabled,
-            plugins: plugins ?? self.plugins,
-            validation: validation ?? self.validation,
-            logger: logger ?? self.logger
-        )
+        let builder = OptionsBuilder()
+        builder.setOptions(self)
+        builderBlock(builder)
+        return builder.build()
     }
-
-    @objc func withOverrides(disabled: NSNumber? = nil,
-                             plugins: [Plugin]? = nil,
-                             validation: ValidationOptions? = nil,
-                             logger: Logger? = nil
+    
+    // Convenience builder for ObjC, in Swift use Options() with optional params
+    @objc public class func builderBlock(
+        _ builderBlock: (OptionsBuilder) -> Void
     ) -> Options {
-        self.withOverrides(environment: self.environment,
-                           disabled: disabled,
-                           plugins: plugins,
-                           validation: validation,
-                           logger: logger
-                           )
+        let builder = OptionsBuilder()
+        builderBlock(builder)
+        return builder.build()
     }
 }
-
-public extension Options {
-    func withOverrides(environment: Environment? = nil,
-                       disabled: Bool? = nil,
-                       plugins: [Plugin]? = nil,
-                       validation: ValidationOptions? = nil,
-                       logger: Logger? = nil
-    ) -> Options {
-        self.withOverrides(
-            environment: environment ?? self.environment,
-            disabled: disabled.map{ NSNumber(booleanLiteral: $0)},
-            plugins: plugins,
-            validation: validation,
-            logger: logger)
-     }
-}
-
 
 @objc (ITLItlyOptionsBuilder) open class OptionsBuilder: NSObject {
-    @objc public var environment: Environment = .development
-    @objc public var disabled: Bool = false
-    @objc public var plugins: [Plugin] = []
-    @objc public var logger: Logger? = nil
-    @objc public var validation: ValidationOptions
-
-    @objc public override init() {
-        self.validation = ValidationOptions(trackInvalid: environment == .production, errorOnInvalid: environment != .production)
-        super.init()
+    @objc public var environment: Environment = DEFAULT_ENVIRONMENT
+    @objc public var disabled: Bool = DEFAULT_DISABLED
+    @objc public var plugins: [Plugin] = DEFAULT_PLUGINS
+    @objc public var logger: Logger? = DEFAULT_LOGGER
+    @objc public var validation: ValidationOptions? = DEFAULT_VALIDATION_OPTIONS
+    
+    @objc public func setOptions(_ options: Options) {
+        self.disabled = options.disabled
+        self.environment = options.environment
+        self.plugins = options.plugins
+        self.validation = options.validation
+        self.logger = options.logger
     }
-
+    
     @objc public func build() -> Options {
         return Options(environment: environment,
                        disabled: disabled,
                        plugins: plugins,
-                       validation: validation, logger: logger)
+                       validation: validation,
+                       logger: logger)
     }
 }
+
