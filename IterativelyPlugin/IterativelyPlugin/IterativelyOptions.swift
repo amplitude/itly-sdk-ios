1fabf618b49c3f44b038885e9213c4cf156b1cd3
Justin Fiedler <justin@iterative.ly>
2020-11-11 22:28:11 +0000

    Cleaned up IterativelyOptionsBuilder implementation

--- IterativelyPlugin/IterativelyPlugin/IterativelyOptions.swift
+++ IterativelyPlugin/IterativelyPlugin/IterativelyOptions.swift
@@ -12,14 +12,14 @@ public let MS_IN_S: Int = 1000
 public let S_IN_M: Int = 60
 public let M_IN_H: Int = 60
 
-public let OMIT_VALUES: Bool = false
-public let BATCH_SIZE: Int = 100
-public let FLUSH_QUEUE_SIZE: Int = 10
-public let FLUSH_INTERVAL_SIZE: Int = 10000
-public let MAX_RETRIES: Int = 25 // ~1 day
-public let DELAY_INITIAL_MILLIS: Int = 10 * MS_IN_S // 10 seconds
-public let DELAY_MAXIMUM_MILLIS: Int = 1 * MS_IN_S * S_IN_M * M_IN_H // = 1 hr
-public let DISABLED: Bool = false
+public let DEFAULT_OMIT_VALUES: Bool = false
+public let DEFAULT_BATCH_SIZE: Int = 100
+public let DEFAULT_FLUSH_QUEUE_SIZE: Int = 10
+public let DEFAULT_FLUSH_INTERVAL_SIZE: Int = 10000
+public let DEFAULT_MAX_RETRIES: Int = 25 // ~1 day
+public let DEFAULT_DELAY_INITIAL_MILLIS: Int = 10 * MS_IN_S // 10 seconds
+public let DEFAULT_DELAY_MAXIMUM_MILLIS: Int = 1 * MS_IN_S * S_IN_M * M_IN_H // = 1 hr
+public let DEFAULT_DISABLED: Bool = false
 
 @objc(ITLIterativelyOptions) public class IterativelyOptions: NSObject {
     @objc public let disabled: Bool
@@ -32,14 +32,14 @@ public let DISABLED: Bool = false
     @objc public let delayMaximumMillis: Int
 
     @objc public init(
-        omitValues: Bool = OMIT_VALUES,
-        batchSize: Int = BATCH_SIZE,
-        flushQueueSize: Int = FLUSH_QUEUE_SIZE,
-        flushIntervalMs: Int = FLUSH_INTERVAL_SIZE,
-        maxRetries: Int = MAX_RETRIES,
-        delayInitialMillis: Int = DELAY_INITIAL_MILLIS,
-        delayMaximumMillis: Int = DELAY_MAXIMUM_MILLIS,
-        disabled: Bool = DISABLED
+        omitValues: Bool = DEFAULT_OMIT_VALUES,
+        batchSize: Int = DEFAULT_BATCH_SIZE,
+        flushQueueSize: Int = DEFAULT_FLUSH_QUEUE_SIZE,
+        flushIntervalMs: Int = DEFAULT_FLUSH_INTERVAL_SIZE,
+        maxRetries: Int = DEFAULT_MAX_RETRIES,
+        delayInitialMillis: Int = DEFAULT_DELAY_INITIAL_MILLIS,
+        delayMaximumMillis: Int = DEFAULT_DELAY_MAXIMUM_MILLIS,
+        disabled: Bool = DEFAULT_DISABLED
     ) {
         self.disabled = disabled;
         self.omitValues = omitValues
@@ -55,30 +55,33 @@ public let DISABLED: Bool = false
     @objc(builderBlock:) public class func builderBlock(
         builderBlock: (IterativelyOptionsBuilder) -> Void
     ) -> IterativelyOptions {
-        let o = IterativelyOptionsBuilder()
-        builderBlock(o)
-        return IterativelyOptions(
-            omitValues: o.omitValues != nil ? o.omitValues!.boolValue : OMIT_VALUES,
-            batchSize: o.batchSize != nil ? o.batchSize!.intValue : BATCH_SIZE,
-            flushQueueSize: o.flushQueueSize != nil ? o.flushQueueSize!.intValue : FLUSH_QUEUE_SIZE,
-            flushIntervalMs: o.flushIntervalMs != nil ? o.flushIntervalMs!.intValue : FLUSH_INTERVAL_SIZE,
-            maxRetries: o.maxRetries != nil ? o.maxRetries!.intValue : MAX_RETRIES,
-            delayInitialMillis: o.delayInitialMillis != nil ? o.delayInitialMillis!.intValue : DELAY_INITIAL_MILLIS,
-            delayMaximumMillis: o.delayMaximumMillis != nil ? o.delayMaximumMillis!.intValue : DELAY_MAXIMUM_MILLIS,
-            disabled: o.disabled != nil ? o.disabled!.boolValue : DISABLED
-        )
+        let builder = IterativelyOptionsBuilder()
+        builderBlock(builder)
+        return builder.build();
     }
 }
 
 
 // Convenience for ObjC, in Swift use IterativelyOptions() with optional params
-@objc(ITLIterativelyOptionsBuilder) public class IterativelyOptionsBuilder: NSObject {
-    @objc public var disabled: NSNumber? = nil
-    @objc public var omitValues: NSNumber? = nil
-    @objc public var batchSize: NSNumber? = nil
-    @objc public var flushQueueSize: NSNumber? = nil
-    @objc public var flushIntervalMs: NSNumber? = nil
-    @objc public var maxRetries: NSNumber? = nil
-    @objc public var delayInitialMillis: NSNumber? = nil
-    @objc public var delayMaximumMillis: NSNumber? = nil
+@objc (ITLIterativelyOptionsBuilder) open class IterativelyOptionsBuilder: NSObject {
+    @objc public var disabled: Bool = DEFAULT_DISABLED
+    @objc public var omitValues: Bool = DEFAULT_OMIT_VALUES
+    @objc public var batchSize: Int = DEFAULT_BATCH_SIZE
+    @objc public var flushQueueSize: Int = DEFAULT_FLUSH_QUEUE_SIZE
+    @objc public var flushIntervalMs: Int = DEFAULT_FLUSH_INTERVAL_SIZE
+    @objc public var maxRetries: Int = DEFAULT_MAX_RETRIES
+    @objc public var delayInitialMillis: Int = DEFAULT_DELAY_INITIAL_MILLIS
+    @objc public var delayMaximumMillis: Int = DEFAULT_DELAY_MAXIMUM_MILLIS
+    
+    @objc public func build() -> IterativelyOptions {
+        return IterativelyOptions(
+            omitValues: self.omitValues,
+            batchSize: self.batchSize,
+            flushQueueSize: self.flushQueueSize,
+            flushIntervalMs: self.flushIntervalMs,
+            maxRetries: self.maxRetries,
+            delayInitialMillis: self.delayInitialMillis,
+            delayMaximumMillis: self.delayMaximumMillis,
+            disabled: self.disabled)
+    }
 }
