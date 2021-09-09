d84d81eb1ac6d7d8a03f8f9f16d2dc87455b9fc9
Justin Fiedler <justin@iterative.ly>
2021-09-09 23:09:17 +0000

    Pass userId correctly to plugins in identify

--- Sdk/Sdk/Itly.swift
+++ Sdk/Sdk/Itly.swift
@@ -12,24 +12,24 @@ import Foundation
     private var context: Event? = nil
 
     private var isDisabled: Bool { config?.disabled ?? true }
-    
+
     @objc public func load(_ context: Properties?, options: Options?) {
         let config = options ?? Options()
-        
+
         guard self.config == nil else {
             self.config.logger?.error("Error: Itly is already loaded. [Itly.instance load] should only be called once.")
             return
         }
-        
+
         guard !config.disabled else {
             config.logger?.info("Itly disabled = true")
             return
         }
-        
+
         if (context != nil) {
             self.context = Event(name: "context", properties: context)
         }
-        
+
         self.config = config
         self.config.logger?.debug("Itly load")
         self.config.logger?.debug("Itly \(config.plugins.count) plugins are enabled")
@@ -38,7 +38,7 @@ import Foundation
             try plugin.safeLoad(config);
         }
     }
-    
+
     @objc public func alias(_ userId: String, previousId: String?) {
         guard !isDisabled else { return }
 
@@ -49,7 +49,7 @@ import Foundation
             try plugin.safePostAlias(userId, previousId: previousId);
         }
     }
-    
+
     @objc public func identify(_ userId: String?, properties: Properties?) {
         guard !isDisabled else { return }
 
@@ -58,13 +58,13 @@ import Foundation
             event: Event(name: "identify", properties: properties),
             includeContext: false,
             method: { (p, e) in
-                try p.safeIdentify(nil, properties: e)
+                try p.safeIdentify(userId, properties: e)
             },
             postMethod: { (p, e, validation) in
-                try p.safePostIdentify(nil, properties: e, validationResults: validation)
+                try p.safePostIdentify(userId, properties: e, validationResults: validation)
             })
     }
-    
+
     @objc public func group(_ groupId: String, properties: Properties?) {
         guard !isDisabled else { return }
 
@@ -79,7 +79,7 @@ import Foundation
                 try p.safePostGroup(nil, groupId: groupId, properties: e, validationResults: validation)
             })
     }
-    
+
     @objc public func track(_ event: Event) {
         guard !isDisabled else { return }
 
@@ -94,7 +94,7 @@ import Foundation
                 try p.safePostTrack(nil, event: e, validationResults: validation)
             })
     }
-    
+
     @objc public func reset() {
         guard !isDisabled else { return }
 
@@ -102,7 +102,7 @@ import Foundation
             try plugin.safeReset();
         }
     }
-    
+
     @objc public func flush() {
         guard !isDisabled else { return }
 
@@ -110,7 +110,7 @@ import Foundation
             try plugin.safeFlush();
         }
     }
-    
+
     @objc public func shutdown() {
         guard !isDisabled else { return }
 
@@ -118,20 +118,20 @@ import Foundation
             try plugin.safeShutdown();
         }
     }
-    
+
     private func validate(_ event: Event) -> [ValidationResponse] {
         guard !config.validation.disabled else {
             return []
         }
-        
+
         var validation: [ValidationResponse] = []
         runOnAllPlugins(op: "validate") { (plugin) in
             validation.append(try plugin.safeValidate(event));
         }
-        
+
         return validation
     }
-    
+
     private func validateAndRunOnAllPlugins(
         op: String,
         event: Event,
@@ -141,40 +141,40 @@ import Foundation
     ) {
         let contextValidation = (includeContext && self.context != nil) ? validate(self.context!) : []
         let isContextValid = contextValidation.valid
-        
+
         let eventValidation = validate(event)
         let isEventValid = eventValidation.valid
-        
+
         let isValid = isContextValid && isEventValid;
-        
+
         let combinedEvent = (includeContext && self.context != nil)
             ? event.mergeProperties(self.context!)
             : event
-        
+
         if (isValid || config.validation.trackInvalid) {
             runOnAllPlugins(op: op, method: { plugin in
                 try method(plugin, combinedEvent)
             })
         }
-        
+
         let validation = contextValidation + eventValidation
-        
+
         // Log any errors
         validation.filter{ !$0.valid }.forEach{ invalidResult in
             config.logger?.error("Validation error for \(combinedEvent.name): \(invalidResult.message ?? "")")
         }
-        
+
         // Run post method
         runOnAllPlugins(op: op, method: { plugin in
             try postMethod(plugin, combinedEvent, validation)
         })
-        
+
         // Throw error is requested
         if (!isValid && config.validation.errorOnInvalid) {
             fatalError(validation.first{ !$0.valid }?.message ?? "Unknown error validating \(event.name)")
         }
     }
-    
+
     private func runOnAllPlugins(op: String, method: (Plugin) throws -> Void) {
         config.plugins.forEach { plugin in
             do {
